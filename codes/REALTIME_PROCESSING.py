3389b158227a808996120797f02fa50a1d99c150
claudiopacha <claudiomonteiro@protonmail.com>
2019-05-30 07:47:55 -0300

    2019-05-29 data

---
+++ codes/REALTIME_PROCESSING.py
@@ -0,0 +1,11 @@
+'''
+collect data from PCR source
+
+compares to local database
+    - time of situacao PENDENTE, PREPARACAO, EXECUCAO, CADASTRADA, ATENDIDA, SITUACAO 
+    - TIME IN 15 MINUTES
+    - FLOW OF SITUATIONS
+    - date on data X date captured
+'''
+
+# FALAR COM PESSOAL DO IFPE PRA GERAR UM COLETOR E ARMAZENAR 
